34fefddbb050802390ca8b834994101bc273cc35
Adrian <adriangonzalesval@gmail.com>
2023-02-28 19:51:33 -0500

    fix: fixed build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,52 +1,42 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ConfigureShadowRelocation
-
 plugins {
-    java
-    id("net.kyori.blossom") version "1.3.1"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    `java-library`
     `maven-publish`
+    signing
+    alias(libs.plugins.blossom)
+    alias(libs.plugins.shadow)
 }
 
 repositories {
-    mavenLocal()
     maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://repo.fvdh.dev/releases")
     maven("https://jitpack.io")
     maven("https://repo.alessiodp.com/releases/")
 }
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(16))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
     withSourcesJar()
     withJavadocJar()
 }
 
-val url = property("url") as String ?: ""
-val id = property("id") as String ?: "chatregulator"
-val configurate = property("configurate-version") as String
-val geantyref = property("geantyref-version") as String
-val caffeine = property("caffeine-version") as String
-
 dependencies {
-    compileOnly("org.spongepowered:configurate-hocon:$configurate")
-    compileOnly("com.github.ben-manes.caffeine:caffeine:$caffeine")
-    compileOnly("org.jetbrains:annotations:24.0.0")
-    shadow("net.byteflux:libby-velocity:1.1.5")
+    compileOnly(libs.configurate)
+    compileOnly(libs.caffeine)
+    shadow(libs.libby)
 
-    compileOnly("com.github.4drian3d:MiniPlaceholders:1.3.1")
+    compileOnly(libs.miniplaceholders)
 
-    compileOnly("com.velocitypowered:velocity-api:3.1.2-SNAPSHOT")
-    annotationProcessor("com.velocitypowered:velocity-api:3.1.2-SNAPSHOT")
+    compileOnly(libs.velocity)
+    annotationProcessor(libs.velocity)
 
-    testImplementation("org.slf4j:slf4j-api:2.0.6")
-    testImplementation("org.spongepowered:configurate-hocon:$configurate")
-    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation(libs.slf4j)
+    testImplementation(libs.configurate)
+    testImplementation(platform(libs.junit.bom))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.mockito:mockito-core:5.1.1")
-    testImplementation("com.velocitypowered:velocity-api:3.1.2-SNAPSHOT")
-    testImplementation("com.github.ben-manes.caffeine:caffeine:$caffeine")
+    testImplementation(libs.mockito)
+    testImplementation(libs.velocity)
+    testImplementation(libs.caffeine)
 }
 
 
@@ -54,31 +44,23 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = group as String
-            artifactId = id
+            artifactId = "chatregulator-api"
             version = version
             from(components["java"])
         }
     }
 }
 
-tasks.withType<Javadoc> {
-    (options as StandardJavadocDocletOptions).links(
-        "https://jd.adventure.kyori.net/api/4.11.0/",
-        "https://jd.adventure.kyori.net/text-minimessage/4.11.0/",
-        "https://jd.papermc.io/velocity/3.0.0/"
-    )
-}
-
 blossom {
     replaceTokenIn("src/main/java/me/dreamerzero/chatregulator/utils/Constants.java")
-	replaceToken("{name}", rootProject.name)
-    replaceToken("{id}", id)
-	replaceToken("{version}", version)
-	replaceToken("{description}", description)
-    replaceToken("{url}", url)
-    replaceToken("{configurate}", configurate)
-    replaceToken("{geantyref}", geantyref)
-    replaceToken("{caffeine}", caffeine)
+    replaceToken("{name}", rootProject.name)
+    replaceToken("{id}", property("id"))
+    replaceToken("{version}", version)
+    replaceToken("{description}", description)
+    replaceToken("{url}", property("url"))
+    replaceToken("{configurate}", libs.versions.configurate)
+    replaceToken("{geantyref}", libs.versions.geantyref)
+    replaceToken("{caffeine}", libs.versions.caffeine)
 }
 
 tasks {
@@ -86,32 +68,27 @@ tasks {
         dependsOn(shadowJar)
     }
     shadowJar {
-        dependsOn(getByName("relocateShadowJar") as ConfigureShadowRelocation)
         minimize()
-        archiveFileName.set("ChatRegulator.jar")
-        configurations = listOf(project.configurations.shadow.get())
-    }
-
-    create<ConfigureShadowRelocation>("relocateShadowJar") {
-        target = shadowJar.get()
-        prefix = "me.dreamerzero.chatregulator.libs"
+        archiveFileName.set("ChatRegulator-${project.version}.jar")
     }
     test {
         useJUnitPlatform()
         testLogging {
-		    events("passed", "skipped", "failed")
-	    }
+            events("passed", "failed")
+        }
     }
 
     compileJava {
-        options.release.set(16)
+        options.release.set(17)
+        options.encoding = "UTF-8"
     }
 
     javadoc {
         options.encoding = Charsets.UTF_8.name()
+        (options as StandardJavadocDocletOptions).links(
+            "https://jd.adventure.kyori.net/api/4.12.0/",
+            "https://jd.adventure.kyori.net/text-minimessage/4.12.0/",
+            "https://jd.papermc.io/velocity/3.0.0/"
+        )
     }
-}
-
-tasks.withType<JavaCompile>() {
-    options.encoding = "UTF-8"
 }
