fcd2ecb7364ede1bde9e2027c182ff2741ad2bf5
Adrian <adriangonzalesval@gmail.com>
2023-05-11 22:50:10 -0500

    test: Added SyntaxCheck Allowed Commands Test

--- plugin/src/test/java/io/github/_4drian3d/chatregulator/modules/checks/SyntaxTest.java
+++ plugin/src/test/java/io/github/_4drian3d/chatregulator/modules/checks/SyntaxTest.java
@@ -3,8 +3,11 @@ package io.github._4drian3d.chatregulator.modules.checks;
 import io.github._4drian3d.chatregulator.api.checks.SyntaxCheck;
 import io.github._4drian3d.chatregulator.utils.TestsUtils;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -18,5 +21,15 @@ class SyntaxTest {
     void syntaxCheck(String command){
         SyntaxCheck check = SyntaxCheck.builder().build();
         assertTrue(check.check(TestsUtils.dummyPlayer(), command).isDenied());
+    }
+
+    @Test
+    void allowedCommand() {
+        String command = "minecraft:tell 4drian3d hola!";
+        SyntaxCheck check = SyntaxCheck.builder()
+                .allowedCommands(List.of("minecraft"))
+                .build();
+
+        assertTrue(check.check(TestsUtils.dummyPlayer(), command).isAllowed());
     }
 }
