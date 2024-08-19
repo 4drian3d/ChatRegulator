d54535f0380b78596a99fcc5a77244940fe4e08e
skbeh <60107333+skbeh@users.noreply.github.com>
2024-08-19 11:54:54 +0000

    Add unicode normalization and apply Formatter to every message

--- api/src/main/java/io/github/_4drian3d/chatregulator/api/utils/Replacer.java
+++ api/src/main/java/io/github/_4drian3d/chatregulator/api/utils/Replacer.java
@@ -1,5 +1,6 @@
 package io.github._4drian3d.chatregulator.api.utils;
 
+import java.text.Normalizer;
 import java.util.Objects;
 
 import org.jetbrains.annotations.NotNull;
@@ -38,11 +39,17 @@ public final class Replacer {
      */
     public static @NotNull String addFinalDot(@NotNull final String string) {
         Objects.requireNonNull(string);
-        if (string.length() <= 1 || string.charAt(string.length() - 1) == '.') {
+        if (string.length() <= 1 || string.length() >= 256 || string.charAt(string.length() - 1) == '.') {
             return string;
         }
 
         return string + ".";
+    }
+
+    public static @NotNull String unicodeNormalize(@NotNull final String string, @NotNull final Normalizer.Form form) {
+        Objects.requireNonNull(string);
+        final String normalized = Normalizer.normalize(string, form);
+        return normalized.length() > 256 ? normalized.substring(0, 256) : normalized;
     }
 
     /**
@@ -52,6 +59,6 @@ public final class Replacer {
      * @return the string converted
      */
     public static @NotNull String applyFormat(final @NotNull String string) {
-        return firstLetterUppercase(addFinalDot(string));
+        return firstLetterUppercase(addFinalDot(unicodeNormalize(string, Normalizer.Form.NFC)));
     }
 }
--- plugin/src/test/java/io/github/_4drian3d/chatregulator/modules/ReplacerTest.java
+++ plugin/src/test/java/io/github/_4drian3d/chatregulator/modules/ReplacerTest.java
@@ -4,6 +4,8 @@ import io.github._4drian3d.chatregulator.api.utils.Replacer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.text.Normalizer;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ReplacerTest {
@@ -26,6 +28,17 @@ class ReplacerTest {
         String replaced = Replacer.addFinalDot(original);
 
         assertEquals(replaced, expected);
+    }
+
+    @Test
+    @DisplayName("Unicode Normalize")
+    void unicodeNormalize() {
+        String original = "\u1100\uAC00\u11A8";
+        String expected = "\u1100\uAC01";
+
+        String replaced = Replacer.unicodeNormalize(original, Normalizer.Form.NFC);
+
+        assertEquals(expected, replaced);
     }
 
     @Test
