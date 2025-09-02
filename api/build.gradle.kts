2a2156a77addd6f9621d370d37fea1eaea82819c
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-09-02 17:16:33 -0500

    ci: Updated maven publication

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,15 +1,14 @@
+import java.time.LocalDate
+
 plugins {
     `java-library`
-    `maven-publish`
-    signing
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
-    withSourcesJar()
-    withJavadocJar()
 }
 
 dependencies {
@@ -34,62 +33,43 @@ tasks {
     }
 }
 
-//publishing {
-//    publications {
-//        create<MavenPublication>("maven") {
-//            repositories {
-//                maven {
-//                    credentials(PasswordCredentials::class)
-//                    val central = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-//                    val snapshots = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-//                    if (project.version.toString().endsWith("SNAPSHOT")) {
-//                        name = "SonatypeSnapshots"
-//                        setUrl(snapshots)
-//                    } else {
-//                        name = "OSSRH"
-//                        setUrl(central)
-//                    }
-//                }
-//            }
-//            from(components["java"])
-//            pom {
-//                url.set("https://github.com/4drian3d/ChatRegulator")
-//                licenses {
-//                    license {
-//                        name.set("GNU General Public License version 3 or later")
-//                        url.set("https://opensource.org/licenses/GPL-3.0")
-//                    }
-//                }
-//                scm {
-//                    connection.set("scm:git:https://github.com/4drian3d/ChatRegulator.git")
-//                    developerConnection.set("scm:git:ssh://git@github.com/4drian3d/ChatRegulator.git")
-//                    url.set("https://github.com/4drian3d/ChatRegulator")
-//                }
-//                developers {
-//                    developer {
-//                        id.set("4drian3d")
-//                        name.set("Adrian Gonzales")
-//                        email.set("adriangonzalesval@gmail.com")
-//                    }
-//                }
-//                issueManagement {
-//                    name.set("GitHub")
-//                    url.set("https://github.com/4drian3d/ChatRegulator/issues")
-//                }
-//                ciManagement {
-//                    name.set("GitHub Actions")
-//                    url.set("https://github.com/4drian3d/ChatRegulator/actions")
-//                }
-//                name.set(project.name)
-//                description.set(project.description)
-//                url.set("https://github.com/4drian3d/ChatRegulator")
-//            }
-//            artifactId = "chatregulator-api"
-//        }
-//    }
-//}
-//signing {
-//    useGpgCmd()
-//    sign(configurations.archives.get())
-//    sign(publishing.publications["maven"])
-//}
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(project.group as String, "miniplaceholders-api", project.version as String)
+
+    pom {
+        name.set(project.name)
+        description.set(project.description)
+        inceptionYear.set(LocalDate.now().year.toString())
+        url.set("https://github.com/4drian3d/ChatRegulator")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("4drian3d")
+                name.set("Adrian Gonzales")
+                email.set("adriangonzalesval@gmail.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:https://github.com/4drian3d/ChatRegulator.git")
+            developerConnection.set("scm:git:ssh://git@github.com/4drian3d/ChatRegulator.git")
+            url.set("https://github.com/4drian3d/ChatRegulator")
+        }
+        ciManagement {
+            name.set("GitHub Actions")
+            url.set("https://github.com/4drian3d/ChatRegulator")
+        }
+        issueManagement {
+            name.set("GitHub")
+            url.set("https://github.com/4drian3d/ChatRegulator/issues")
+        }
+    }
+}
+
